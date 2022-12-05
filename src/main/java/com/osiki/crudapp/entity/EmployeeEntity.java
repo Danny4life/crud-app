ab49d36e7f9602ff3f77ec95a511f77cfa182d5b
Segun Osiki <osiki.segun@gmail.com>
2022-12-05 22:07:26 +0100

    added employee entity

--- src/main/java/com/osiki/crudapp/entity/EmployeeEntity.java
+++ src/main/java/com/osiki/crudapp/entity/EmployeeEntity.java
@@ -1,4 +1,20 @@
 package com.osiki.crudapp.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Data
+@Table(name = "employees")
 public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }

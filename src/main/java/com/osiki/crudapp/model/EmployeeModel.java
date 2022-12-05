f436af2e8f92f52953cd31984dfca0df4fc363b8
Segun Osiki <osiki.segun@gmail.com>
2022-12-05 22:09:05 +0100

    added employee model

--- src/main/java/com/osiki/crudapp/model/EmployeeModel.java
+++ src/main/java/com/osiki/crudapp/model/EmployeeModel.java
@@ -1,4 +1,16 @@
 package com.osiki.crudapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeModel {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
